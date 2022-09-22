d42d0e06ed56a5ad9e432f138dc89ea6edd96246
KUMAR PIYUSH <107334940+KumarPiyush89@users.noreply.github.com>
2022-09-22 22:19:37 +0530

    Add files via upload

---
+++ CreationAndInsertionDataIn BST.cpp
@@ -0,0 +1,325 @@
+/* Operator Overloading*/
+#include<iostream>
+#include<cmath>
+using namespace std;
+class FractionNumber
+{
+	private:
+		int num;
+		int dnum;
+	public:
+		FractionNumber(int num=0,int dnum=1)
+		{
+			this->num=num;
+			if(dnum==0)dnum=1;
+			this->dnum=dnum;
+			if(this->dnum<0)
+			{
+				this->num*=-1;
+				this->dnum*=-1;
+			}
+		}
+		FractionNumber(double op2)
+		{
+			FractionNumber ob2;
+			float decimalPoint=op2-floor(op2);
+			int td=0,newInt=0;
+			while(decimalPoint)
+			{
+				decimalPoint*=(float)10;
+				newInt=newInt*10+(int)decimalPoint;
+				decimalPoint-=(float)((int)decimalPoint);
+				td++;
+			}
+			int dnum=1;
+			while(td--)
+			dnum*=10;
+			
+			newInt+=((int)op2)*dnum;
+			ob2.set(newInt,dnum);
+			
+		}
+	void set(int num,int dnum=1)
+	{
+		this->num=num;
+			if(dnum==0)dnum=1;
+			this->dnum=dnum;
+			if(this->dnum<0)
+			{
+				this->num*=-1;
+				this->dnum*=-1;
+			}
+	}
+	void setNum(int num)
+	{
+		this->num=num;
+	}
+	void setdNum(int dnum)
+	{
+		if(dnum==0)dnum=1;
+		this->dnum=dnum;
+		if(this->dnum<0)
+		{
+			this->num*=-1;
+			this->dnum*=-1;
+		}
+	}
+	int getNum()
+	{
+		return num;
+	}
+	int getDnum()
+	{
+		return dnum;
+	}
+	void show()
+	{
+		cout<<num<<"/"<<dnum;
+	}
+	void show(char *name)
+	{
+		cout<<name<<"=";
+		show();
+	}
+	
+	FractionNumber add(const FractionNumber &ob2)
+	{
+		FractionNumber ob3;
+		//find the LCM of both denum
+		int a,b;
+		a=this->dnum;
+		b=ob2.dnum;
+		while(a!=b)
+		{
+			if(a<b)
+			a+=this->dnum;
+			else
+			b+=ob2.dnum;
+		}
+		ob3.dnum=a;
+		ob3.num=a/this->dnum*this->num+a/ob2.dnum*ob2.num;
+		return ob3;
+	}
+	//overloading + for adding two fraction number
+		FractionNumber operator+(const FractionNumber &ob2)
+	{
+		FractionNumber ob3;
+		//find the LCM of both denum
+		int a,b;
+		a=this->dnum;
+		b=ob2.dnum;
+		while(a!=b)
+		{
+			if(a<b)
+			a+=this->dnum;
+			else
+			b+=ob2.dnum;
+		}
+		ob3.dnum=a;
+		ob3.num=a/this->dnum*this->num+a/ob2.dnum*ob2.num;
+		return ob3;
+	}	
+	
+	//for subtraction FractionNumber-FractionNumber	
+	FractionNumber operator-(const FractionNumber &ob2)
+	{
+		FractionNumber ob3;
+		//find the LCM of both denum
+		int a,b;
+		a=this->dnum;
+		b=ob2.dnum;
+		while(a!=b)
+		{
+			if(a<b)
+			a+=this->dnum;
+			else
+			b+=ob2.dnum;
+		}
+		ob3.dnum=a;
+		ob3.num=a/this->dnum*this->num-a/ob2.dnum*ob2.num;
+		return ob3;
+	}
+	
+	//for multiplication FractionNumber*FractionNumber
+	FractionNumber operator*(const FractionNumber &ob2)
+	{
+		FractionNumber ob3;
+		ob3.num=this->num*ob2.num;
+		ob3.dnum=this->dnum*ob2.dnum;
+		//find hcf of ob3's num and dnum.
+		int a,b;
+		a=ob3.num;
+		b=ob3.dnum;
+		while(a!=b)
+		{
+			if(a>b)
+			a-=b;
+			else
+			b-=a;
+		}
+		if(a!=1)
+		{
+			ob3.dnum=ob3.dnum/a;
+			ob3.num=ob3.num/a;
+		}
+		return ob3;
+	}
+	
+	//for division FractionNumber/FractionNumber
+	FractionNumber operator/(const FractionNumber &ob2)
+	{
+		FractionNumber ob3;
+		ob3.num=this->num*ob2.dnum;
+		ob3.dnum=this->dnum*ob2.num;
+		//find hcf of ob3's num and dnum.
+		int a,b;
+		a=ob3.num;
+		b=ob3.dnum;
+		while(a!=b)
+		{
+			if(a>b)
+			a-=b;
+			else
+			b-=a;
+		}
+		if(a!=1)
+		{
+			ob3.dnum=ob3.dnum/a;
+			ob3.num=ob3.num/a;
+		}
+		return ob3;
+	}
+			
+	//overloading add For FractionNumber+int
+	FractionNumber add(int op2)
+	{
+		FractionNumber ob2(op2);
+		return add(ob2);
+	}
+	//overloading + for FractionNumber+int
+	FractionNumber operator+(int ob2)
+	{
+		return add(ob2);
+	}
+	//overloading add For FractionNumber+float
+	FractionNumber operator+(double op2)
+	{
+		FractionNumber ob2(op2);
+		return add(ob2);
+	}
+	
+	//Overloading + for int+FractionNumber
+	/* It can't be performed using member function
+	because, when operator function is called, always left operand of expression
+	is invoking object and right operand is passed as argument to operator function.
+	And int type is not an object so a method can't be called using int data
+	
+	C++ supports non member function so,we can solve this problem using non member function
+	*/
+			
+};
+//overloading + operator for (int+FractionNumber), using non member function
+FractionNumber operator+(int x,FractionNumber& ob)
+{
+	return ob+x;
+}
+FractionNumber operator+(float f,FractionNumber& ob)
+{
+	return ob+f;
+}
+
+//overloading << for ostream<<FractionNumber
+ostream& operator<<( ostream &out,FractionNumber& ob)
+{
+	out<<ob.getNum()<<"/"<<ob.getDnum();
+	return out;
+}
+//overloading >> for istream>>FractionNumber
+void operator>>(istream &in,FractionNumber& ob)
+{
+	int n,d;
+	cout<<"Enter value of Numerator: ";
+	in>>n;
+	cout<<"Enter value of Denominator: ";
+	in>>d;
+	ob.set(n,d);
+}
+	
+
+ 
+int main()
+{
+	FractionNumber ob1(5,2),ob2(7);
+	cout<<endl<<"ob1= "<<ob1;
+	cout<<endl<<"ob2= "<<ob2;
+	
+	ob2.setdNum(-2);
+	cout<<endl<<"ob2= "<<ob2;
+
+	FractionNumber ob3=ob1.add(ob2);
+	cout<<endl<<"ob3= "<<ob1<<"+"<<ob2<<"= "<<ob3;
+
+	FractionNumber ob4(5,3),ob5(13,7),ob6;
+	ob6=ob4+ob5;
+	cout<<endl<<"ob6="<<ob4<<" + "<<ob5<<"= "<<ob6<<endl;
+	
+	ob1.set(4,2);
+	cout<<endl<<"ob1= "<<ob1;
+	ob2.set(6,2);
+	cout<<endl<<"ob2= "<<ob2;
+	ob3=ob1-ob2;
+	cout<<endl<<"ob3= "<<ob1<<" - "<<ob2<<" = "<<ob3;
+
+	ob1.set(4,2);
+	cout<<endl<<endl<<"ob1= "<<ob1;
+	ob2.set(6,2);
+	cout<<endl<<"ob2= "<<ob2;
+	ob3=ob1*ob2;
+		cout<<endl<<"ob3= "<<ob1<<" * "<<ob2<<" = "<<ob3;
+	
+	ob1.set(4,2);
+	cout<<endl<<endl<<"ob1= "<<ob1;
+	ob2.set(6,2);
+	cout<<endl<<"ob2= "<<ob2;
+	ob3=ob1/ob2;
+		cout<<endl<<"ob3= "<<ob1<<" / "<<ob2<<" = "<<ob3;
+
+	FractionNumber n1(22,7);
+	cout<<endl<<endl<<"n1="<<n1<<".";
+	FractionNumber n2;
+	n2=n1+1;
+	cout<<endl<<n1<<"+1="<<n2<<endl<<endl;
+	
+	//cin>>ob6;
+	cout<<endl<<"ob6= "<<ob6;
+	float f=0;
+	cout<<endl<<"Enter value of float: ";
+	cin>>f;
+	//cout<<endl<<"ob6= "<<ob6;
+	ob6=ob6+f;
+	//cout<<" + "<<f<<" = "<<ob6;
+	return 0;
+}
+
+/*
+Syntax Of Operator Function
+
+retunType operator#(argument)
+{
+//function defination
+}
+at the place of # ,overloading operator symbol should be written
+ex:
+for operloading + function name will be:
+return type operator+(argument)
+{
+}
+
+for overloadin * function name will be:
+returnType operator*(argument)
+{
+}
+
+*/
+
+
